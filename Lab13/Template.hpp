b00d8d2bd6c017da50c37209ec4781aac3c4e87d
EGregg05 <egregg208@gmail.com>
2025-04-15 17:13:06 -0700

    Defined all functions in the template classes

--- Lab13/Template.hpp
+++ Lab13/Template.hpp
@@ -19,6 +19,16 @@ public:
 		return this->data;
 	}
 
+	ListNode< NODETYPE >* getNext() const
+	{
+		return this->nextPtr;
+	}
+
+	void setNext(ListNode<NODETYPE>* newnode)
+	{
+		this->nextPtr = newnode;
+	}
+
 private:
 
 	NODETYPE data; // data
@@ -33,27 +43,85 @@ class List
 {
 public:
 
-	List(); // constructor
+	List() // constructor
+	{
+		this->firstPtr = nullptr;
+		this->lastPtr = nullptr;
+	}
 
-	~List(); // destructor
+	~List() // destructor
+	{
+		while (this->firstPtr != nullptr)
+		{
+			ListNode< NODETYPE >* pCur = this->firstPtr;
+			this->firstPtr = this->firstPtr->getNext();
+			delete pCur;
+		}
+	}
 
-	void insertAtFront(const NODETYPE& newData);
+	void insertAtFront(const NODETYPE& newData)
+	{
+		ListNode< NODETYPE >* pNew = this->getNewNode(newData);
+		pNew->setNext(this->firstPtr);
+		this->firstPtr = pNew;
+	}
 
-	void insertAtBack(const NODETYPE& newData);
+	void insertAtBack(const NODETYPE& newData)
+	{
+		ListNode< NODETYPE >* pNew = this->getNewNode(newData);
+		this->lastPtr->setNext(pNew);
+	}
 
-	bool removeFromFront(NODETYPE& removedData);
+	bool removeFromFront(NODETYPE& removedData)
+	{
+		removedData = this->firstPtr->getData();
 
-	bool removeFromBack(NODETYPE& removedData);
+		ListNode< NODETYPE >* pCur = this->firstPtr;
+		this->firstPtr = this->firstPtr->getNext;
+		delete this->pCur;
+	}
 
-	bool isEmpty() const;
+	bool removeFromBack(NODETYPE& removedData)
+	{
+		removedData = this->lastPtr->getData();
 
-	void print() const;
+		ListNode< NODETYPE >* pCur = this->lastPtr;
+		this->lastPtr = nullptr;
+		delete this->pCur;
+	}
+
+	bool isEmpty() const
+	{
+		if (this->firstPtr == nullptr)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	void print() const
+	{
+		ListNode< NODETYPE >* pCur = this->firstPtr;
+		
+		while (pCur != nullptr)
+		{
+			std::cout << pCur->getData << std::endl;
+
+			pCur = pCur->getNext();
+		}
+	}
 
 private:
 
 	ListNode< NODETYPE >* firstPtr; // pointer to first node
 	ListNode< NODETYPE >* lastPtr; // pointer to last node
 	// Utility function to allocate a new node
-	ListNode< NODETYPE >* getNewNode(const NODETYPE& newData);
+	ListNode< NODETYPE >* getNewNode(const NODETYPE& newData)
+	{
+		return ListNode<NODETYPE>* newNode = new ListNode<newData>;
+	}
 
 };
